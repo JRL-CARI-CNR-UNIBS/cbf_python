e87dacb74b6b5095a40430ea69832f0e5fbb2b85
fedep999 <fede99215@gmail.com>
2026-02-18 15:11:09 +0100

    minor fix

--- cbf_python/scripts/optimize_parameters.py
+++ cbf_python/scripts/optimize_parameters.py
@@ -9,31 +9,43 @@ from Command_bridge.fake_command_bridge import FakeCommandBridge
 from Controller.optimal_cbf_task_controller import BCFOptimalController, ControllerConfig
 from multiprocessing import Process, Queue
 from queue import Empty
+from Controller.dynamic_params_controllers import (PolynomialControllerConfig, PolynomialOptimalController,
+                                                   StocasticalControllerConfig, StocasticalOptimalController)
+from pathlib import Path
+import pandas as pd
+
 # Database connection (for dashboard)
 POSTGRES_URL = "postgresql+psycopg2://optuna:optuna_pw@localhost:5432/optuna_db"
 
+
+
 def make_objective():
     def objective(trial):
-        lambda_pos = trial.suggest_float("lambda_pos", 5e2, 5e4, log=True)
-        lambda_vel = trial.suggest_float("lambda_vel", 1e-3, 1e3, log=True)
-        lambda_scaling = trial.suggest_float("lambda_scaling", 10, 1e4, log=True)
-        lambda_acc = trial.suggest_float("lambda_acc", 1e-14, 1e-2, log=True)
-        gamma = trial.suggest_float("gamma", 1, 10, log=True)
-        delta = trial.suggest_float("delta_deg", 0.1, 5, log=True)
+
+        cfg = ControllerConfig(Tc=2e-3)
+        delta = 4.5
+
+        cfg.delta_q_max[0:2] = np.deg2rad(np.array([1, 1], dtype=np.float64) * delta)
+        cfg.delta_q_max[2:4] = np.deg2rad(np.array([1, 1], dtype=np.float64) * delta) * 2
+        cfg.delta_q_max[4:6] = np.deg2rad(np.array([1, 1], dtype=np.float64) * delta) * 4
+
 
         try:
-           c_f, mean_scale, viol_rate, mean_trajectory_error = run_episode_with_timeout(
-                lambda_pos, lambda_vel, lambda_scaling, lambda_acc, gamma, delta, Tc=2e-3, duration=500.0,
-                timeout=600
+            cost, viol_rate, mean_scale, mean_traj_err = run_episode_with_timeout(
+                cfg=cfg, Tc=2e-3, duration=1000.0,
+                timeout=6000
             )
+            trial.set_user_attr("mean_scale", mean_scale)
+            trial.set_user_attr("viol_rate", viol_rate)
+            trial.set_user_attr("trajectory_error", mean_traj_err)
         except TimeoutError:
-            # For directions: [minimize, maximize, minimize, minimize]
-            return 0
-        trial.set_user_attr("mean_scale", mean_scale)
-        trial.set_user_attr("viol_rate", viol_rate)
-        trial.set_user_attr("trajectory_error", mean_trajectory_error)
-        return c_f
+                # For directions: [minimize, maximize, minimize, minimize]
+            return 0.0
+
+        return cost
+
     return objective
+
 
 # ----------------- STATIC INITIALIZATION (only once) -----------------
 def compute_ee_pose(q, model, data, ee_frame_id):
@@ -47,8 +59,8 @@ def compute_ee_pose(q, model, data, ee_frame_id):
     pin.updateFramePlacements(model, data)
 
     T_ee = data.oMf[ee_frame_id]  # SE3 from world (o) to frame (f=tool0)
-    p = T_ee.translation          # 3D position
-    R = T_ee.rotation             # 3x3 rotation matrix
+    p = T_ee.translation  # 3D position
+    R = T_ee.rotation  # 3x3 rotation matrix
     return p, R, T_ee
 
 
@@ -70,19 +82,19 @@ UR10E_JOINTS = [
     "ur10e_wrist_3_joint",
 ]
 
-Tc =2e-3
+Tc = 2e-3
 gen_cfg = ControllerConfig(Tc=Tc)
 # # Basic planner reused across trials
 q = home.copy()
-q10 = np.array([ 31.0, -78.0, 115.0, -127.0, 86.0, -32.0])*np.pi/180.0
-q20 =  np.array([ 31.0, -83.0, 98.0, -110.0, 86.0, -32.0])*np.pi/180.0
-q22 =  np.array([ 40.0, -126.0, 141.0, -100.0, 86.0, 45.0])*np.pi/180.0
-q25 =  np.array([ 130.0, -100.0, 125.0, -115.0, 94.0, -20.0])*np.pi/180.0
-q30 =  np.array([ 136.0, -60.0, 90.0, -122.0, 90.0, 45.0])*np.pi/180.0
-q40 =  np.array([ 134.0, -65.0, 70.0, -90.0, 90.0, 45.0])*np.pi/180.0
-gen_cfg.Dq_max = gen_cfg.Dq_max*0.25
-gen_cfg.DDq_max = gen_cfg.DDq_max*0.2
-planner = SegmentedJointTrap(Dq_max=gen_cfg.Dq_max*0.25, DDq_max=gen_cfg.DDq_max*0.25)
+q10 = np.array([31.0, -78.0, 115.0, -127.0, 86.0, -32.0]) * np.pi / 180.0
+q20 = np.array([31.0, -83.0, 98.0, -110.0, 86.0, -32.0]) * np.pi / 180.0
+q22 = np.array([40.0, -126.0, 141.0, -100.0, 86.0, 45.0]) * np.pi / 180.0
+q25 = np.array([130.0, -100.0, 125.0, -115.0, 94.0, -20.0]) * np.pi / 180.0
+q30 = np.array([136.0, -60.0, 90.0, -122.0, 90.0, 45.0]) * np.pi / 180.0
+q40 = np.array([134.0, -65.0, 70.0, -90.0, 90.0, 45.0]) * np.pi / 180.0
+gen_cfg.Dq_max = gen_cfg.Dq_max * 0.25
+gen_cfg.DDq_max = gen_cfg.DDq_max * 0.2
+planner = SegmentedJointTrap(Dq_max=gen_cfg.Dq_max * 0.25, DDq_max=gen_cfg.DDq_max * 0.25)
 # CONFIG 1
 planner.addWayPoint(q)
 planner.addWayPoint(q10)
@@ -94,7 +106,6 @@ planner.addWayPoint(q30)
 planner.addWayPoint(q40)
 planner.addWayPoint(q30)
 planner.addWayPoint(q)
-
 
 T_total = planner.computeTime()
 model_wrapper = loadSharework(UR10E_JOINTS)
@@ -126,31 +137,16 @@ for name in cartesian_configs:
     p, R, T_ee = compute_ee_pose(configs[name], model, data, tool_frame_id)
     cartesian_configs[name] = p.tolist()
 
-scaling_threshold = 0.5  
-#-------------------- EVALUATION FUNCTION --------------------
-def run_episode(lambda_pos, lambda_vel, lambda_scaling, lambda_acc, gamma, delta, Tc=2e-3, duration=500.0):
+scaling_threshold = 0.5
 
+
+# -------------------- EVALUATION FUNCTION --------------------
+def run_episode(Tc=2e-3, duration=500.0, cfg=PolynomialControllerConfig()):
     home = np.array([90.0, -140.0, 140.0, -90.0, 90.0, 0.0]) * np.pi / 180.0
 
-    cfg = ControllerConfig(Tc=Tc)
-    cfg.lambda_pos = lambda_pos
-    cfg.lambda_vel = lambda_vel
-    cfg.lambda_scaling = lambda_scaling
-    cfg.lambda_acc = lambda_acc
-    cfg.delta_q_max[0:2] = np.deg2rad(np.array([1,1], dtype=np.float64) * delta)
-    cfg.delta_q_max[2:4] = np.deg2rad(np.array([1,1], dtype=np.float64) * delta)*2
-    cfg.delta_q_max[4:6] = np.deg2rad(np.array([1,1], dtype=np.float64) * delta)*4
-    cfg.Dq_max = cfg.Dq_max*0.25
-    cfg.DDq_max = cfg.DDq_max*0.2
-    cfg.gamma = gamma
-
-    ctrl = BCFOptimalController(model_wrapper=model_wrapper, cfg=cfg, useCbf=True)
-   
-    # quat = pin.Quaternion(0.814, 0.178, 0.535, 0.137)
-    # quat.normalize()
-    # R = quat.toRotationMatrix()
-
-    # T_wc = pin.SE3(R, np.array([0.108, -0.883, 2.351]))
+    cfg.Dq_max = cfg.Dq_max * 0.25
+    cfg.DDq_max = cfg.DDq_max * 0.2
+    ctrl = BCFOptimalController(model_wrapper=model_wrapper, cfg=cfg, useCbf=True, keypoint_to_log=-1)
 
     bridge = FakeCommandBridge(
         UR10E_JOINTS,
@@ -173,7 +169,7 @@ def run_episode(lambda_pos, lambda_vel, lambda_scaling, lambda_acc, gamma, delta
 
     low_scale_count = 0
     while t < duration:
-        obs_pos, obs_vel, obs_acc = bridge.getObstacles(elapsed = t)
+        obs_pos, obs_vel, obs_acc = bridge.getObstacles(elapsed=t)
         nominal_q, nominal_Dq, nominal_DDq = planner.getMotionLaw(trajectory_time % T_total)
         try:
             out = ctrl.step(
@@ -194,16 +190,16 @@ def run_episode(lambda_pos, lambda_vel, lambda_scaling, lambda_acc, gamma, delta
                 enable_lap_count = True
             for i in range(len(cartesian_configs.values())):
                 q_wp = list(cartesian_configs.values())[i]
-                if  np.linalg.norm(q_wp - end_eff_pos) < 2e-03 and prec_target != i:
+                if np.linalg.norm(q_wp - end_eff_pos) < 2e-03 and prec_target != i:
                     on_target_count += 1
                     prec_target = i
                     break
         except Exception:
             # Penalize infeasible or divergent QP
             print("QP failed")
-            return 1.0, -1.0, 1000.0, 1.0
+            return 1.0, -1.0, 10.0, 1.0
         t += Tc
-        
+
         if out["h_min"] < 0 and out["vr_min"] < -1e-3:
             violations += 1
         sum_scale += out["Dtrajectory_time"]
@@ -212,19 +208,17 @@ def run_episode(lambda_pos, lambda_vel, lambda_scaling, lambda_acc, gamma, delta
         if out["Dtrajectory_time"] < scaling_threshold:
             low_scale_count += 1
         trajectory_time = out["trajectory_time"]
-        
 
-       
         time.sleep(1e-4)  # To avoid locking issues in multiprocessing
 
-
-    #on_target_rate = on_target_count/(n_wp * ((lap_count)+ ((trajectory_time % T_total)/T_total)))
-    lap_count = lap_count + ((trajectory_time % T_total)/T_total)
+    # on_target_rate = on_target_count/(n_wp * ((lap_count)+ ((trajectory_time % T_total)/T_total)))
+    lap_count = lap_count + ((trajectory_time % T_total) / T_total)
     viol_rate = violations / max(1, nsteps)
     mean_scale = sum_scale / max(1, nsteps)
     mean_trajectory_error = trajectory_error_sum / max(1, nsteps)
     low_scale_rate = low_scale_count / max(1, nsteps)
-    return mean_scale - 10.0 * viol_rate -mean_trajectory_error, mean_scale, viol_rate, low_scale_rate
+    cost_function = mean_scale - 10*viol_rate -mean_trajectory_error
+    return cost_function, viol_rate, mean_scale, mean_trajectory_error
 
 
 def _run_episode_worker(args, kwargs, q):
@@ -234,6 +228,7 @@ def _run_episode_worker(args, kwargs, q):
         q.put(("ok", result))
     except Exception as e:
         q.put(("err", repr(e)))
+
 
 def run_episode_with_timeout(*args, timeout=600, **kwargs):
     """
@@ -262,8 +257,8 @@ def run_episode_with_timeout(*args, timeout=600, **kwargs):
     else:
         raise RuntimeError(f"run_episode failed in worker: {payload}")
 
-# -------------------- OPTUNA OPTIMIZATION --------------------
 
+# -------------------- OPTUNA OPTIMIZATION --------------------
 
 
 storage = optuna.storages.RDBStorage(
@@ -273,28 +268,45 @@ storage = optuna.storages.RDBStorage(
         "pool_size": 40,
         "max_overflow": 20,
     },
-    heartbeat_interval=30,     # worker pings DB every 30s
-    grace_period=120,          # declare failed if no ping for 120s
+    heartbeat_interval=30,  # worker pings DB every 30s
+    grace_period=120,  # declare failed if no ping for 120s
     # failed_trial_callback=RetryFailedTrialCallback(max_retry=1),
 )
-
-# h_dic = {
-#     "h_negative": [-1000.0, 0.0],
-#     "h_q1": [0.0, 0.25],
-#     "h_q2": [0.25, 0.5],
-#     "h_q3": [0.5, 0.75],
-#     "h_positive": [0.75, 1000.0],
-# }
-# for h_key, (h_min, h_max) in h_dic.items():
 
 study = optuna.create_study(
     directions=["maximize"],
     sampler=optunahub.load_module("samplers/auto_sampler").AutoSampler(),
     storage=storage,
-    #load_if_exists=True,
-    study_name=f"params_test_{time.strftime('%Y%m%d-%H%M%S')}",
-)
-study.set_metric_names(["cost_function"])
-study.optimize(make_objective(), n_trials=2500, show_progress_bar=True, n_jobs=30, gc_after_trial=True)
+    # load_if_exists=True,
+    # study_name=f"dynamic_params_polynomial_{time.strftime('%Y%m%d-%H%M%S')}",
+    study_name=f"dynamic_params_polynomial_20260216-094358",
+    load_if_exists=True,
 
-    # print (run_episode(1e3,1e3,1e3,1e-3,5,1))
+)
+study.set_metric_names(["cost function"])
+study.optimize(make_objective(), n_trials=1000, show_progress_bar=True, n_jobs=30, gc_after_trial=True)
+
+# print (run_episode(1e3,1e3,1e3,1e-3,5,1))
+
+
+''' 
+TRIAL BELLI delta variabile
+
+4999
+4780
+4789
+4845
+4706
+4630
+4558
+761
+'''
+
+''' 
+TRIAL BELLI delta fisso
+
+3083
+3289
+3978
+4937
+'''
