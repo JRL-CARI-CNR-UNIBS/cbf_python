a014bed6953b8845b5cb6f92850cae6bbd9314fd
fedep999 <federico.parma@unibs.it>
2026-07-10 12:26:09 +0200

    optimal_case_opt fix

--- cbf_python/scripts/optimize_parameters.py
+++ cbf_python/scripts/optimize_parameters.py
@@ -9,8 +9,9 @@ from Command_bridge.fake_command_bridge import FakeCommandBridge
 from Controller.optimal_cbf_task_controller import BCFOptimalController, ControllerConfig
 from multiprocessing import Process, Queue
 from queue import Empty
-from Controller.dynamic_params_controllers import (PolynomialControllerConfig, PolynomialOptimalController,
-                                                   StocasticalControllerConfig, StocasticalOptimalController)
+# from Controller.dynamic_params_controllers import (PolynomialControllerConfig, PolynomialOptimalController,
+#                                                    StocasticalControllerConfig, StocasticalOptimalController)
+from Controller.optimal_cbf_task_controller import ControllerConfig, BCFOptimalController
 from pathlib import Path
 import pandas as pd
 import os
@@ -51,7 +52,7 @@ def save_data(study):
     # 2. Salvataggio in Append (Coda)
     # ---------------------------------------------------------
 
-    filename = "log_best_trials.csv"
+    filename = "trials.csv"
 
     # Verifica se il file esiste
     file_exists = os.path.isfile(filename)
@@ -83,18 +84,16 @@ def make_objective():
 
 
         try:
-            cost, viol_rate, mean_scale, mean_traj_err = run_episode_with_timeout(
+            viol_rate, mean_scale, mean_traj_err = run_episode_with_timeout(
                 cfg=cfg, Tc=2e-3, duration=150,
                 timeout=6000
             )
-            trial.set_user_attr("mean_scale", mean_scale)
-            trial.set_user_attr("viol_rate", viol_rate)
-            trial.set_user_attr("trajectory_error", mean_traj_err)
+
         except TimeoutError:
                 # For directions: [minimize, maximize, minimize, minimize]
-            return 0.0
+            return 1.0, 0.0, 1.0
 
-        return cost
+        return viol_rate, mean_scale, mean_traj_err
 
     return objective
 
@@ -122,7 +121,7 @@ quat = pin.Quaternion(0.83, 0.185, 0.513, 0.12)
 quat.normalize()
 R = quat.toRotationMatrix()
 
-T_wc = pin.SE3(R, np.array([0.094, -0.93, 2.309]))
+T_wc = pin.SE3(R, np.array([-0.094, -0.93, 2.309]))
 
 home = np.array([90, -140, 140, -90, 90, 0]) * np.pi / 180.0
 UR10E_JOINTS = [
@@ -146,7 +145,7 @@ q30 = np.array([136.0, -60.0, 90.0, -122.0, 90.0, 45.0]) * np.pi / 180.0
 q40 = np.array([134.0, -65.0, 70.0, -90.0, 90.0, 45.0]) * np.pi / 180.0
 gen_cfg.Dq_max = gen_cfg.Dq_max * 0.25
 gen_cfg.DDq_max = gen_cfg.DDq_max * 0.2
-planner = SegmentedJointTrap(Dq_max=gen_cfg.Dq_max * 0.25, DDq_max=gen_cfg.DDq_max * 0.25)
+planner = SegmentedJointTrap(Dq_max=gen_cfg.Dq_max * 0.25, DDq_max=gen_cfg.DDq_max * 0.125)
 # CONFIG 1
 planner.addWayPoint(q)
 planner.addWayPoint(q10)
@@ -193,16 +192,14 @@ scaling_threshold = 0.5
 
 
 # -------------------- EVALUATION FUNCTION --------------------
-def run_episode(Tc=2e-3, duration=500.0, cfg=PolynomialControllerConfig()):
+def run_episode(Tc=2e-3, duration=1500.0, cfg=ControllerConfig()):
     home = np.array([90.0, -140.0, 140.0, -90.0, 90.0, 0.0]) * np.pi / 180.0
 
-    cfg.Dq_max = cfg.Dq_max * 0.25
-    cfg.DDq_max = cfg.DDq_max * 0.2
     ctrl = BCFOptimalController(model_wrapper=model_wrapper, cfg=cfg, useCbf=True, keypoint_to_log=-1)
 
     bridge = FakeCommandBridge(
         UR10E_JOINTS,
-        csv_path="/home/nyquist/projects/cells_ws/src/zed_skeleton_kinematics/csv_files/skeleton_vectors_14_NORMAL_TEST1.csv",
+        csv_path="/home/galileo_gal/projects/python/cbf_python_ws/cbf_python/cbf_python/skeleton_vectors/skeleton_vectors_23.csv",
         Tworld_to_cam=T_wc,
         slowdown_factor=1.0,
         t0=0.0
@@ -269,8 +266,7 @@ def run_episode(Tc=2e-3, duration=500.0, cfg=PolynomialControllerConfig()):
     mean_scale = sum_scale / max(1, nsteps)
     mean_trajectory_error = trajectory_error_sum / max(1, nsteps)
     low_scale_rate = low_scale_count / max(1, nsteps)
-    cost_function = mean_scale - 10*viol_rate -mean_trajectory_error
-    return cost_function, viol_rate, mean_scale, mean_trajectory_error
+    return viol_rate, mean_scale, mean_trajectory_error
 
 
 def _run_episode_worker(args, kwargs, q):
@@ -324,19 +320,19 @@ storage = optuna.storages.RDBStorage(
     grace_period=120,  # declare failed if no ping for 120s
     # failed_trial_callback=RetryFailedTrialCallback(max_retry=1),
 )
-
+sampler = optuna.samplers.NSGAIIISampler()
 study = optuna.create_study(
-    direction="maximize",
+    directions=["minimize", "maximize", "minimize"],
     storage=storage,
     # load_if_exists=True,
-    sampler=CmaEsSampler(restart_strategy="ipop"), # 'ipop' riavvia se si blocca in un minimo locale
-    study_name=f"params_test_{time.strftime('%Y%m%d-%H%M%S')}",
+    sampler=sampler,
+    study_name=f"params_optimal_{time.strftime('%Y%m%d-%H%M%S')}",
     # study_name=f"dynamic_params_polynomial_20260216-094358",
     load_if_exists=True,
 
 )
-study.set_metric_names(["cost function"])
-study.optimize(make_objective(), n_trials=3000, show_progress_bar=True, n_jobs=30, gc_after_trial=True)
+study.set_metric_names(["viol_rate", "mean_scaling", "mean_trajectory_error"])
+study.optimize(make_objective(), n_trials=5000, show_progress_bar=True, n_jobs=30, gc_after_trial=True)
 save_data(study)
 # print (run_episode(1e3,1e3,1e3,1e-3,5,1))
 
